47917cebf8f16d4b28c0c747b65d4186894f083a
kenjihatasa <kenjinke0723@gmail.com>
2017-04-21 04:39:02 +0000

    want

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,43 @@ class ApplicationController < ActionController::Base
       redirect_to login_url
     end
   end
+  
+  private
+
+  def read(result)
+    code = result.code
+    name = result['itemName']
+    url = result.url
+    image_url = result['mediumImageUrls'].first['imageUrl'].gsub('?_ex=128x128', '')
+
+    return {
+      code: code,
+      name: name,
+      url: url,
+      image_url: image_url,
+    }
+  end
+end
+app/controllers/items_controller.rb
+
+class ItemsController < ApplicationController
+  before_action :require_user_logged_in
+
+  def new
+    @items = []
+
+    @keyword = params[:keyword]
+    if @keyword
+      results = RakutenWebService::Ichiba::Item.search({
+        keyword: @keyword,
+        imageFlag: 1,
+        hits: 20,
+      })
+
+      results.each do |result|
+        item = Item.find_or_initialize_by(read(result))
+        @items << item
+      end
+    end
+  end
 end
