552eeb382bd2f81fceb94800c6ac4a55c8dd3c95
kenjihatasa <kenjinke0723@gmail.com>
2017-04-21 04:50:00 +0000

    want

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,26 +27,3 @@ class ApplicationController < ActionController::Base
     }
   end
 end
-app/controllers/items_controller.rb
-
-class ItemsController < ApplicationController
-  before_action :require_user_logged_in
-
-  def new
-    @items = []
-
-    @keyword = params[:keyword]
-    if @keyword
-      results = RakutenWebService::Ichiba::Item.search({
-        keyword: @keyword,
-        imageFlag: 1,
-        hits: 20,
-      })
-
-      results.each do |result|
-        item = Item.find_or_initialize_by(read(result))
-        @items << item
-      end
-    end
-  end
-end
